7d916ec485f447f4cf59a5f31f255d3f37777eb2
Kaustubh0511 <84490159+Kaustubh0511@users.noreply.github.com>
2023-03-22 21:32:54 +0530

    Initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+const react = require("react");
+
+function Header() {
+  return (
+    <header>
+      <h1>Noter</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Components/Notes.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Footer";
+
+const react = require("react");
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Learning React</h1>
+      <p>
+        React is a JavaScript library for building user interfaces. Learn what
+        React is all about on our homepage or in the tutorial.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
